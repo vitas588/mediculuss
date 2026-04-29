fa6acd3bbed67eb175d00bfc206bc179858abc3d
vitas588 <vitas1012coop@gmail.com>
2026-04-29 16:40:25 +0300

    Brevo SMTP

--- mediculus/settings.py
+++ mediculus/settings.py
@@ -167,10 +167,10 @@ if DEBUG:
     }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
-EMAIL_USE_TLS = False
+EMAIL_HOST = config('EMAIL_HOST', default='smtp-relay.brevo.com')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_USE_SSL = config('EMAIL_USE_SSL', default=False, cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
-DEFAULT_FROM_EMAIL = config('EMAIL_HOST_USER', default='noreply@mediculus.ua')
+DEFAULT_FROM_EMAIL = config('EMAIL_HOST_USER', default='')
